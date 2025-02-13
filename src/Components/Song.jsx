7d3e739d5f12b145b7df9b2c7ee7c4cd429c8393
Sharangdhar <sharangdhar7984@gmail.com>
2025-02-13 15:32:46 +0530

    bug fix in Song component

--- src/Components/Song.jsx
+++ src/Components/Song.jsx
@@ -1,20 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export const Song=({fn,song})=>{
-    const[playerFlag,setplayerFlag]=useState(false);
-    const showPlayer=()=>{
-        fn(true,song);
-       
-    }
-    return(<div className="row">
-        <div className="col-4">
-            <img src={song.artworkUrl100} alt=""/>
-        </div>
-        <div className="col-4">
-            {song.artistName}{song.trackName}
-        </div>
-        <div className="col-4"> 
-            <button onClick={showPlayer} className="btn btn-primary">Play Song</button>
-        </div>
-    </div>)
-}
+export const Song = ({ fn, song }) => {
+  //const[playerFlag,setplayerFlag]=useState(false);
+  const showPlayer = () => {
+    fn(true, song);
+  };
+  return (
+    <div className="row">
+      <div className="col-4">
+        <img src={song.artworkUrl100} alt="" />
+      </div>
+      <div className="col-4">
+        {song.artistName}
+        {song.trackName}
+      </div>
+      <div className="col-4">
+        <button onClick={showPlayer} className="btn btn-primary">
+          Play Song
+        </button>
+      </div>
+    </div>
+  );
+};
